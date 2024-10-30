799f4ed85ea323709b6f4b19f51885642bc15b9e
oracle22 <sandermi@uia.no>
2024-10-30 20:39:05 +0100

    task 3.1 done

--- module_3/task_3-1/task_3-1.mjs
+++ module_3/task_3-1/task_3-1.mjs
@@ -5,30 +5,80 @@ initPrintOut(document.getElementById("txtOut"));
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
+let wakeUpTime = 6;
+if (wakeUpTime===7) {
+  printOut("I can take the bus to school.")
+}else if(wakeUpTime===8) {
+
+
+  printOut ("I can take the train to school") 
+}else if (wakeUpTime===6){
+  printOut("I have to take the car to school")
 }
+  
+
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let number= 6 
+if (number <6){
+  printOut("positive")
+} else if (number>6){
+  printOut("False")
+} else {
+  printOut("zero")
+}
+
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let imagesize= Math.floor(Math.random()*8)+1;
+printOut("image size"+ imagesize +"MP");
+const minimumsize=4
+if (imagesize>minimumsize){
+  printOut("Tank you!")
+}else {
+  printOut("image to small  :(");
+}
+
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+printOut("image size"+ imagesize +"MP");
+
+const maximagesize=6
+if (imagesize>=minimumsize && imagesize<=maximagesize){
+  printOut("Tank you!")
+}else if(imagesize>maximagesize){
+  printOut("image to big  :(");
+}else if (imagesize<minimumsizephoto){
+  printOut("Image to small :(")
+}
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const monthList =["January", "February", "Mars", "April", "Mai",
+  "Jun", "Juli", "August", "September", "October", "November", "December"];
+  const noOfMonth = monthList.length;
+  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; 
+  const monthindex= Math.floor(Math.random() * noOfMonth);
+  const monthname= monthList[monthindex]
+  const days= daysInMonth[monthindex]
+  printOut("Selected month" +monthname);
+  if (monthname.toLowerCase().includes("r")){
+    printOut("You need vitamin D")
+
+  } else
+  printOut("You dont need vitamin D")
+  printOut("There are"+days+ "days in"+ monthname)
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
@@ -39,5 +89,12 @@ printOut(newLine);
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+if (monthname==="March"|| monthname==="Mai"){
+  printOut("the gallery is closed")
+}else if (monthname==="April"){
+  printOut("Gallery is temporarily open in a nearby bulding")
+}else {
+  printOut("Gallery is open")
+}
+
 printOut(newLine);
--- module_3/task_3-2/task_3-2.mjs
+++ module_3/task_3-2/task_3-2.mjs
@@ -8,13 +8,36 @@ printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Put your code below here!
+Create a program that can guess a number between 1 and 60. Declare a variable and assign it a value, for
+example, 45. Let the computer "guess" by generating a random number. Use a "while" loop and the
+"random" function. Keep the "while" loop running as long as the "guessed number" is incorrect. Print the
+number once the "while" loop has completed. You do not need to print anything while the "while" loop is in
+progress.*/
+const answerNumber = 45;
+let guessNumber = 0 
+while(answerNumber !== guessNumber){
+guessNumber = Math.ceil (Math.random() * 60); 
+}
+printOut("Guess Number =" + guessNumber.toString());
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Put your code below here!
+Take the program from part 2 and expand it to guess a number between 1 and one million. Print the
+number of guesses as well as the number of milliseconds it took to guess the number. HINT: Use the
+Date.now() function to measure time.*/
+guessNumber = 0
+let guessCount= 
+const startTime= Date.now();
+while (answerNumber !== guessNumber){
+    guessCount++;
+    guessNumber = Math.ceil (Math.random()*1000000);
+}
+const endTime= Date.now();
+const timeUsed= endTime-startTime;
+
+printOut("Guess number= " +  guessNumber + "Number of guesses= " + guessCount.toString() + "time used to guess number= "+ timeUsed.toString()+"ms");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
@@ -33,8 +56,56 @@ printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Put your code below here!
+● 1 2 3 4 5 6 (full straight)
+● 3 pairs
+● 2 of a kind and 4 of a kind (tower)
+● All the same (Yahtzee)*/
+printOut("Dice Rolling Extravaganza");
+const d1= Math.ceil(Math.random()*6);
+const d2= Math.ceil(Math.random()*6);
+const d3= Math.ceil(Math.random()*6);
+const d4= Math.ceil(Math.random()*6);
+const d5= Math.ceil(Math.random()*6);
+const d6= Math.ceil(Math.random()*6);
+
+let diceThrow= ""; 
+diceThrow += d1.toString()+ ",";
+diceThrow += d2.toString()+ ",";
+diceThrow += d3.toString()+ ",";
+diceThrow += d4.toString()+ ",";
+diceThrow += d5.toString()+ ",";
+diceThrow += d6.toString();
+ 
+ printOut("diceThrow: " + diceThrow);
+
+const count1= diceThrow.match(/1/g) || "".length; 
+const count2= diceThrow.match(/2/g) || "".length; 
+const count3= diceThrow.match(/3/g) || "".length; 
+const count4= diceThrow.match(/4/g) || "".length; 
+const count5= diceThrow.match(/5/g) || "".length; 
+const count6= diceThrow.match(/6/g) || "".length; 
+
+let diceCount= ""; 
+diceCount += d1.toString()+ ",";
+diceCount += d2.toString()+ ",";
+diceCount += d3.toString()+ ",";
+diceCount += d4.toString()+ ",";
+diceCount += d5.toString()+ ",";
+diceCount += d6.toString();
+ printOut("DiceCount: "+ diceCount);
+
+const equals1 = (diceCount.match(/1/g)||"").length
+const equals6 = (diceCount.match(/6/g)||"").length
+printOut("Equals1"+ equals1.toString());
+printOut("equals6" + equals6.toString()); 
+
+if(equals1===6){
+    printOut("Full straight")
+} else if (equals6===1){
+    printOut("Yatzy!!");
+}
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
